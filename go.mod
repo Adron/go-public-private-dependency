21eb4641c1a8ff9ba35eeef33c4d864a4f414ca2
Adron Hall <adron@thrashingcode.com>
2020-03-17 16:38:10 -0700

    Adding private dependencies.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Adron/go-public-private-dependency
+
+go 1.14
+
+require github.com/Adron/go-private-module-repo v1.0.0
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	say "github.com/Adron/go-private-module-repo"
+)
+
+func main() {
+	result := say.Hello()
+	fmt.Println(result)
+}
